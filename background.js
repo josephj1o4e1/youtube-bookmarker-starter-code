3fbde8e50e97cf83dbfb0288cfdd0b4574d60cae
shchua <shchua@iu.edu>
2022-12-27 20:37:42 -0500

    console.log

--- background.js
+++ background.js
@@ -2,6 +2,8 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
       const queryParameters = tab.url.split("?")[1];
       const urlParameters = new URLSearchParams(queryParameters);
+      console.log("urlParameters!!!!!!!!!!!!");
+      console.log(urlParameters);
   
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
